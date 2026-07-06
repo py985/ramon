7f7b8cb20ef5a5743b55b3c8917b6a7ce2e8cad3
py985 <maxramonslp@gmail.com>
2026-07-05 23:05:38 -0300

    Adiciona main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+        },
+        icon: path.join(__dirname, 'icon.png')
+    });
+    win.loadFile('index.html');
+    Menu.setApplicationMenu(null);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
